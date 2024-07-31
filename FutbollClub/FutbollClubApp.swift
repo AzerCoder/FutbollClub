c4c594c7857b1a96fef75f20241b1d5663f7b23e
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-07-31 15:38:20 +0500

    Initial Commit

---
+++ FutbollClub/FutbollClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutbollClubApp.swift
+//  FutbollClub
+//
+//  Created by A'zamjon Abdumuxtorov on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FutbollClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
